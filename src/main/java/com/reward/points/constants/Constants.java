13e2e099856ed23babb5e399f25a901dccc74833
joshirushikesh141 <72022583+joshirushikesh141@users.noreply.github.com>
2025-01-09 19:44:30 +0530

    Update Constants.java

--- src/main/java/com/reward/points/constants/Constants.java
+++ src/main/java/com/reward/points/constants/Constants.java
@@ -4,5 +4,6 @@ public class Constants {
     public static final int daysInMonths = 30;
     public static int firstRewardLimit = 50;
     public static int secondRewardLimit = 100;
+    public static final String SomethingWentWrong  = "Someting went wrong!! Please check the provided data once...";
     
 }
