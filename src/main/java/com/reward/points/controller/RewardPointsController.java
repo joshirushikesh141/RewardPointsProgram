12e15e8303f9ad6127391a8acda71ac78475ace5
joshirushikesh141 <72022583+joshirushikesh141@users.noreply.github.com>
2025-01-09 19:42:47 +0530

    Update RewardPointsController.java

--- src/main/java/com/reward/points/controller/RewardPointsController.java
+++ src/main/java/com/reward/points/controller/RewardPointsController.java
@@ -2,13 +2,13 @@ package com.reward.points.controller;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.reward.points.constants.Constants;
 import com.reward.points.entity.Customer;
 import com.reward.points.entity.Transaction;
 import com.reward.points.model.Rewards;
@@ -38,30 +39,86 @@ public class RewardPointsController {
     
     private static final Logger logger = LoggerFactory.getLogger(RewardPointsController.class);
     
+    // Save Customer Details
     @PostMapping("/saveCustomerDetails")
-    public ResponseEntity<Customer> saveCustomerDetails(@RequestBody Customer customer){
-    	Customer save = new Customer();
+    public ResponseEntity<String> saveCustomerDetails(@RequestBody Customer customer){
+    	String save = "";
     	try {
-    		save  = rewardPointsService.saveCustomerDetails(customer);
+    		 save = rewardPointsService.saveCustomerDetails(customer);
     		logger.info("save {}",save);
 		} catch (Exception e) {
 			logger.error("exception in saveCustomerDetails {}",e);
+			return new ResponseEntity<>(Constants.SomethingWentWrong,HttpStatus.INTERNAL_SERVER_ERROR);
 		}
         return new ResponseEntity<>(save,HttpStatus.OK);
     }
     
+    // Get Customer Details By customerId
+    @GetMapping("/getCustomerDetails/{customerId}")
+    public ResponseEntity<Customer> getCustomerDetailsByCustomerId(@PathVariable Long customerId){
+    	Customer customer = new Customer();
+    	try {
+    		customer = rewardPointsService.getCustomerDetailsByCustomerId(customerId);
+    		logger.info("getCustomerDetailsByCustomerId customer {}",customer);
+		} catch (Exception e) {
+			logger.error("exception in getCustomerDetailsByCustomerId {}",e);
+			return new ResponseEntity<>(customer,HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+        return new ResponseEntity<>(customer,HttpStatus.OK);
+    }
+    
+    // Update Customer Details By customerId
+    @PutMapping("/updateCustomerDetails/{customerId}")
+    public ResponseEntity<String> updateCustomerDetails(@RequestBody Customer customer,@PathVariable Long customerId){
+    	Customer customerDb = new Customer();
+    	String saveCustomerDetails ="";
+    	try {
+    		customerDb = rewardPointsService.getCustomerDetailsByCustomerId(customerId);
+        	if(customerDb != null) {
+        		customerDb.setCustomerName(customer.getCustomerName());
+        		saveCustomerDetails = rewardPointsService.saveCustomerDetails(customerDb);
+        	}
+		} catch (Exception e) {
+			logger.error("exception in updateCustomerDetails {}",e);
+			return new ResponseEntity<>(Constants.SomethingWentWrong,HttpStatus.NOT_FOUND);
+		}
+    	return new ResponseEntity<>(saveCustomerDetails,HttpStatus.OK);
+    }
+    
+    // Delete Customer Details By customerId
+    @DeleteMapping("/deleteCustomerDetails/{customerId}")
+    public ResponseEntity<String> deleteCustomerDetails(@PathVariable Long customerId){
+    	Customer customerObj = new Customer();
+    	String deleteCustomer = "";
+    	try {
+    		customerObj = rewardPointsService.getCustomerDetailsByCustomerId(customerId);
+        	if(customerObj != null) {
+        		deleteCustomer = rewardPointsService.deleteCustomerDetails(customerId);
+        	}
+		} catch (Exception e) {
+			logger.error("exception in deleteCustomerDetails {}",e);
+			return new ResponseEntity<>(Constants.SomethingWentWrong,HttpStatus.NOT_FOUND);
+		}
+    	return new ResponseEntity<>(deleteCustomer,HttpStatus.OK);
+    }
+    
+    
+    
+    // Save Transaction Details -- Object
     @PostMapping("/saveTransactionDetails")
-    public ResponseEntity<Transaction> saveTransactionDetails(@RequestBody Transaction transaction){
-    	Transaction save = new Transaction();
+    public ResponseEntity<String> saveTransactionDetails(@RequestBody Transaction transaction){
+    	String save = "";
     	try {
     		save = rewardPointsService.saveTransactionDetails(transaction);
     		logger.info("save {}",save);
 		} catch (Exception e) {
 			logger.error("exception in saveTransactionDetails {}",e);
+			return new ResponseEntity<>(Constants.SomethingWentWrong,HttpStatus.INTERNAL_SERVER_ERROR);
 		}
         return new ResponseEntity<>(save,HttpStatus.OK);
     }
     
+    // Save Transaction Details List -- List of Object
     @PostMapping("/saveAllTransactionDetails")
     public ResponseEntity<List<Transaction>> saveAllTransactionDetails(@RequestBody List<Transaction> transaction){
     	List<Transaction> save = new ArrayList<>();
@@ -70,31 +127,12 @@ public class RewardPointsController {
     		logger.info("save {}",save);
 		} catch (Exception e) {
 			logger.error("exception in saveAllTransactionDetails {}",e);
+			return new ResponseEntity<>(save,HttpStatus.INTERNAL_SERVER_ERROR);
 		}
         return new ResponseEntity<>(save,HttpStatus.OK);
     }
-	
-    @GetMapping("/getCustomerDetails/{customerId}")
-    public ResponseEntity<Customer> getCustomerDetailsByCustomerId(@PathVariable Long customerId){
-    	Customer customer = new Customer();
-    	try {
-    		customer = rewardPointsService.getCustomerDetailsByCustomerId(customerId);
-    		 logger.info("getCustomerDetailsByCustomerId customer {}",customer);
-		} catch (Exception e) {
-			logger.error("exception in getCustomerDetailsByCustomerId {}",e);
-		}
-        return new ResponseEntity<>(customer,HttpStatus.OK);
-    }
     
-    @PostMapping("/updateCustomerDetails/{customerId}")
-    public void getCustomerDetails(@RequestBody Customer customer,@PathVariable Long customerId){
-    	Customer customerDb = rewardPointsService.getCustomerDetailsByCustomerId(customerId);
-    	if(customerDb != null) {
-    		customerDb.setCustomerName(customer.getCustomerName());
-    		rewardPointsService.saveCustomerDetails(customerDb);
-    	}
-    }
-    
+    // Get Transaction Details By customerId
     @GetMapping("/getTransactionDetailsByCustomerId/{customerId}")
     public ResponseEntity<List<Transaction>> getTransactionDetailsByCustomerId(@PathVariable Long customerId){
     	 List<Transaction> findByCustomerId = new ArrayList<>();
@@ -103,10 +141,50 @@ public class RewardPointsController {
 		     logger.info("getTransactionDetailsByCustomerId transaction {}",findByCustomerId);
 		} catch (Exception e) {
 			logger.error("exception in findByCustomerId {}",e);
+			return new ResponseEntity<>(findByCustomerId,HttpStatus.INTERNAL_SERVER_ERROR);
 		}
         return new ResponseEntity<>(findByCustomerId,HttpStatus.OK);
     }
+    
+    // Update Transaction Details -- Object
+    @PutMapping("/updateTransactionDetails/{transactionId}")
+    public ResponseEntity<String> updateTransactionDetails(@RequestBody Transaction transaction,@PathVariable Long transactionId){
+    	Transaction transactionObj = new Transaction();
+    	String save = "";
+    	try {
+    		transactionObj = rewardPointsService.getTransactionDetailsByTransactionId(transactionId);
+    		if(transactionObj != null) {
+    			transactionObj.setCustomerId(transaction.getCustomerId());
+    			transactionObj.setTransactionAmount(transaction.getTransactionAmount());
+    			transactionObj.setTransactionDate(transaction.getTransactionDate());
+	    		save = rewardPointsService.saveTransactionDetails(transactionObj);
+	    		logger.info("save {}",save);
+    		}
+		} catch (Exception e) {
+			logger.error("exception in updateTransactionDetails {}",e);
+			return new ResponseEntity<>(Constants.SomethingWentWrong,HttpStatus.NOT_FOUND);
+		}
+        return new ResponseEntity<>(save,HttpStatus.OK);
+    }
+    
+    // Delete Transaction Details By customerId
+    @DeleteMapping("/deleteTransactionDetails/{transactionId}")
+    public ResponseEntity<String> deleteTransactionDetails(@PathVariable Long transactionId){
+    	Transaction transactionObj = new Transaction();
+    	String deleteTransaction = "";
+    	try {
+    		transactionObj = rewardPointsService.getTransactionDetailsByTransactionId(transactionId);
+        	if(transactionObj != null) {
+        		deleteTransaction = rewardPointsService.deleteTransactionDetails(transactionId);
+        	}
+		} catch (Exception e) {
+			logger.error("exception in deleteTransactionDetails {}",e);
+			return new ResponseEntity<>(Constants.SomethingWentWrong,HttpStatus.NOT_FOUND);
+		}
+    	return new ResponseEntity<>(deleteTransaction,HttpStatus.OK);
+    }
 
+    // Get Rewards By customerId
     @GetMapping("/rewards/{customerId}")
     public ResponseEntity<Rewards> getRewardsByCustomerId(@PathVariable Long customerId){
     	Customer customer = rewardPointsService.getCustomerDetailsByCustomerId(customerId);
