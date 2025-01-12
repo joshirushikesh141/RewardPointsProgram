9f6f57230f85b50dfa364a21347f9dc15b7b12d1
joshirushikesh141 <72022583+joshirushikesh141@users.noreply.github.com>
2025-01-12 21:12:31 +0530

    Update Customer.java

--- src/main/java/com/reward/points/entity/Customer.java
+++ src/main/java/com/reward/points/entity/Customer.java
@@ -10,7 +10,7 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "CUSTOMER")
 public class Customer {
-    @Id
+     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "CUSTOMER_ID")
     private Long customerId;
@@ -18,27 +18,47 @@ public class Customer {
     @Column(name = "CUSTOMER_NAME")
     private String customerName;
     
+    @Column(name = "EMAIL_ID")
+    private String emailId;
+
 	public Long getCustomerId() {
 		return customerId;
 	}
+
 	public void setCustomerId(Long customerId) {
 		this.customerId = customerId;
 	}
+
 	public String getCustomerName() {
 		return customerName;
 	}
+
 	public void setCustomerName(String customerName) {
 		this.customerName = customerName;
 	}
-	
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
 	public Customer() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	
-	@Override
-	public String toString() {
-		return "Customer [customerId=" + customerId + ", customerName=" + customerName + "]";
+
+	public Customer(Long customerId, String customerName, String emailId) {
+		super();
+		this.customerId = customerId;
+		this.customerName = customerName;
+		this.emailId = emailId;
 	}
 
+	@Override
+	public String toString() {
+		return "Customer [customerId=" + customerId + ", customerName=" + customerName + ", emailId=" + emailId + "]";
+	}
 }
