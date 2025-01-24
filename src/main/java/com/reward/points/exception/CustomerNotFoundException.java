21ec23c0bf9b067c40d94e9aab792efb924aacb3
joshirushikesh141 <72022583+joshirushikesh141@users.noreply.github.com>
2025-01-24 12:54:33 +0530

    Update CustomerNotFoundException.java

--- src/main/java/com/reward/points/exception/CustomerNotFoundException.java
+++ src/main/java/com/reward/points/exception/CustomerNotFoundException.java
@@ -1,5 +1,11 @@
 package com.reward.points.exception;
 
+/**
+ * Exception thrown when a customer is not found.
+ * This class extends RuntimeException and provides a custom exception message.
+ *
+ * @see RuntimeException
+ */
 @SuppressWarnings("serial")
 public class CustomerNotFoundException extends RuntimeException{
 
