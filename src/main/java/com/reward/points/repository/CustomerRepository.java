118900b336ac2905f1d83b06e5de70081cae8a74
joshirushikesh141 <72022583+joshirushikesh141@users.noreply.github.com>
2025-01-24 12:07:07 +0530

    Update CustomerRepository.java

--- src/main/java/com/reward/points/repository/CustomerRepository.java
+++ src/main/java/com/reward/points/repository/CustomerRepository.java
@@ -3,6 +3,14 @@ package com.reward.points.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Repository interface for performing CRUD operations on the Customer entity.
+ * Extends JpaRepository to provide methods for working with Customer persistence.
+ * 
+ * @see JpaRepository
+ * @see Customer
+ */
+
 import com.reward.points.entity.Customer;
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long>{
